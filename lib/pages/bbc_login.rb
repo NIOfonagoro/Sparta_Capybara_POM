421754a68637d81720205eaf7b184654d8d58f0b
Tech-A25 <tech-a25@Tech-A25s-Air.lan>
2018-07-17 23:36:40 +0100

    First Test working

--- lib/pages/bbc_login.rb
+++ lib/pages/bbc_login.rb
@@ -7,7 +7,7 @@ class BBC_Login
   USERNAME = "username"
   DUMMY_EMAIL = "nick.drastikk@gmail.com"
   PASSWORD_INPUT = "password"
-  DUMMY_PASSWORD = ""
+  DUMMY_PASSWORD = "budokai3"
 
   def fill_email
     fill_in USERNAME, with: DUMMY_EMAIL
@@ -20,5 +20,25 @@ class BBC_Login
   def click_submit
     click_button "Sign in"
     sleep 1
+  end
+
+  def click_username
+    click_on "Username"
+    sleep 1
+  end
+
+  def click_neutral_area
+
+  end
+
+  def click_password
+    click_on "password"
+    sleep 1
+  end
+
+  def username_error
+    # find(:css, "#form-message-username").should be_visible
+    # expect(page).to have_selector('#form-message-username', visible: true)
+  page.has_selector?("#form-message-username")
   end
 end
--- spec/bbc_login_spec.rb
+++ spec/bbc_login_spec.rb
@@ -4,7 +4,7 @@ describe "Testing the BBC" do
 
   context "Testing Login" do
 
-    it "should respond with success when we login with a valid email and password" do
+    xit "should respond with success when we login with a valid email and password" do
       @bbc_site = BBC_Site.new
       @bbc_site.bbc_homepage.visit_homepage
       @bbc_site.bbc_homepage.click_sign_in_link
@@ -15,6 +15,20 @@ describe "Testing the BBC" do
       sleep 1
       @bbc_site.bbc_login.click_submit
       sleep 2
+    end
+
+    it "should respond with success when we login with a valid email and password" do
+      @bbc_site = BBC_Site.new
+      @bbc_site.bbc_homepage.visit_homepage
+      @bbc_site.bbc_homepage.click_sign_in_link
+      sleep 1
+      @bbc_site.bbc_login.find("#user-identifier-input").click
+      sleep 1
+      @bbc_site.bbc_login.find(".field__input--password-toggle").click
+      sleep 1
+
+      expect(@bbc_site.bbc_login.username_error).to be true
+
     end
 
   end
